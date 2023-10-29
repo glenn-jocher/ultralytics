046f2ef7015e59188105b3b8c7971da15a864d5c
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-10-29 20:01:00 +0100

    update

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -26,14 +26,19 @@ def generate_ddp_file(trainer):
     """Generates a DDP file and returns its file name."""
     module, name = f'{trainer.__class__.__module__}.{trainer.__class__.__name__}'.rsplit('.', 1)
 
-    content = f'''overrides = {vars(trainer.args)} \nif __name__ == "__main__":
+    content = f"""
+# Ultralytics Multi-GPU training temp file (should be automatically deleted after use)
+overrides = {vars(trainer.args)}
+
+if __name__ == "__main__":
     from {module} import {name}
     from ultralytics.utils import DEFAULT_CFG_DICT
 
     cfg = DEFAULT_CFG_DICT.copy()
     cfg.update(save_dir='')   # handle the extra key 'save_dir'
     trainer = {name}(cfg=cfg, overrides=overrides)
-    trainer.train()'''
+    results = trainer.train()
+"""
     (USER_CONFIG_DIR / 'DDP').mkdir(exist_ok=True)
     with tempfile.NamedTemporaryFile(prefix='_temp_',
                                      suffix=f'{id(trainer)}.py',
