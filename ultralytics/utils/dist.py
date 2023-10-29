8ed596f2625a5455c9a1b47ce325ac6c1c5fee46
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-10-29 19:39:14 +0100

    update

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -52,10 +52,13 @@ def generate_ddp_command(world_size, trainer):
     import __main__  # noqa local import to avoid https://github.com/Lightning-AI/lightning/issues/15218
     if not trainer.resume:
         shutil.rmtree(trainer.save_dir)  # remove the save_dir
+    print('SYS ARGV', sys.argv)
     file = str(Path(sys.argv[0]).resolve())
+    print('FILE:', file)
     safe_pattern = re.compile(r'^[a-zA-Z0-9_. /\\-]{1,128}$')  # allowed characters and maximum of 100 characters
     if not (safe_pattern.match(file) and Path(file).exists() and file.endswith('.py')):  # using CLI
         file = generate_ddp_file(trainer)
+    print('SAFE_FILE:', file)
     dist_cmd = 'torch.distributed.run' if TORCH_1_9 else 'torch.distributed.launch'
     port = find_free_network_port()
     cmd = [sys.executable, '-m', dist_cmd, '--nproc_per_node', f'{world_size}', '--master_port', f'{port}', file]
