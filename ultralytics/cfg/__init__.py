3f157f6a0c44b15063a72c71ab5b0867401eb7b9
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-11-18 03:01:00 +0100

    update

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -159,6 +159,7 @@ def get_save_dir(args, name=None):
         save_dir = increment_path(Path(project) / name, exist_ok=args.exist_ok if RANK in (-1, 0) else True)
 
     print('GET_SAVE_DIR2', save_dir)
+    LOGGER.info('GET_SAVE_DIR3', 'check mark here: ✅')
     return Path(save_dir)
 
 
