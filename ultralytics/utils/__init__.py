68b99fc17a6c7691ea7c9bc739f7df24ed2f485f
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-11-18 03:27:43 +0100

    update

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -226,8 +226,7 @@ def plt_settings(rcparams=None, backend='Agg'):
 
 def set_logging(name=LOGGING_NAME, verbose=True):
     """Sets up logging for the given name with UTF-8 encoding support."""
-    rank = int(os.getenv('RANK', -1))  # rank in world for Multi-GPU trainings
-    level = logging.INFO if verbose and rank in {-1, 0} else logging.ERROR
+    level = logging.INFO if verbose and RANK in {-1, 0} else logging.ERROR  # rank in world for Multi-GPU trainings
 
     # Configure the console (stdout) encoding to UTF-8
     if WINDOWS:  # for Windows
@@ -242,12 +241,11 @@ def set_logging(name=LOGGING_NAME, verbose=True):
     logger.setLevel(level)
     logger.addHandler(stream_handler)
     logger.propagate = False
+    return logger
 
 
 # Set logger
-set_logging(LOGGING_NAME, verbose=VERBOSE)  # run before defining LOGGER
-LOGGER = logging.getLogger(LOGGING_NAME)  # define globally (used in train.py, val.py, detect.py, etc.)
-
+LOGGER = set_logging(LOGGING_NAME, verbose=VERBOSE)  # define globally (used in train.py, val.py, detect.py, etc.)
 for logger in 'sentry_sdk', 'urllib3.connectionpool':
     logging.getLogger(logger).setLevel(logging.CRITICAL)
 
@@ -255,29 +253,6 @@ for logger in 'sentry_sdk', 'urllib3.connectionpool':
 def emojis(string=''):
     """Return platform-dependent emoji-safe version of string."""
     return string.encode().decode('ascii', 'ignore') if WINDOWS else string
-
-
-class EmojiFilter(logging.Filter):
-    """
-    A custom logging filter class for removing emojis in log messages.
-
-    This filter is particularly useful for ensuring compatibility with Windows terminals that may not support the
-    display of emojis in log messages.
-    """
-
-    def filter(self, record):
-        """Filter logs by emoji unicode characters on windows."""
-        record.msg = emojis(record.msg)
-        return super().filter(record)
-
-
-# Set logger
-# set_logging(LOGGING_NAME, verbose=VERBOSE)  # run before defining LOGGER
-# LOGGER = logging.getLogger(LOGGING_NAME)  # define globally (used in train.py, val.py, detect.py, etc.)
-# if WINDOWS:  # emoji-safe logging
-#     LOGGER.addFilter(EmojiFilter())
-# for logger in 'sentry_sdk', 'urllib3.connectionpool':
-#     logging.getLogger(logger).setLevel(logging.CRITICAL)
 
 
 class ThreadingLocked:
